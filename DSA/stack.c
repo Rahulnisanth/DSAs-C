936ea84afa643d37f7b66c8d26d3f25b2040e8c9
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-09-08 19:11:20 +0530

    added stack SLL

--- DSA/stack.c
+++ DSA/stack.c
@@ -58,6 +58,81 @@ int main()
     return 0;
 }
 
+// STACK IMPLEMENTATION USING SLL :
+#include <stdlib.h>
+#include <stdio.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head, *tail; // Head & Tail pointers
+void push(int new_value)
+{
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->data = new_value;
+    new_node->next = NULL;
+
+    if (head == NULL)
+        head = tail = new_node;
+    else
+    {
+        tail->next = new_node;
+        tail = new_node;
+    }
+}
+void pop()
+{
+    struct node *tag = head;
+    struct node *prev;
+    if (head == NULL)
+    {
+        printf("The stack is empty!\n");
+        return;
+    }
+    if (tag->next == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    else
+    {
+        while (tag->next != NULL)
+        {
+            prev = tag;
+            tag = tag->next;
+        }
+        prev->next = NULL;
+        printf("Popped last element %d in the stack!\n", tag->data);
+        free(tag);
+    }
+}
+void display()
+{
+    struct node *temp = head;
+    printf("The elements in the stack are:\n");
+    while (temp != NULL)
+    {
+        printf("%d <- ", temp->data);
+        temp = temp->next;
+    }
+}
+// MAIN DRIVE FUNCTION :
+int main()
+{
+    // pushing elements in stack...
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    // popping the last element...
+    pop();
+    // printing the stack...
+    display();
+    return 0;
+}
+
 // PALINDROME USING STACK:
 #include <stdio.h>
 #include <stdlib.h>
